8fe648e06c148ca10c3c5e401f8080ebb8b1ac3f
killtime <killtime@loqoonet.com>
2011-12-15 19:56:19 -0500

    second commit

---
+++ exitDOC.CODe/firstcode.cpp
@@ -0,0 +1,11 @@
+#include <iostream.h>
+int main(void)
+
+{   int jump;
+     cout<< "Come up and C++ me some time.";
+     cout<< "\n";
+     cin>> jump;
+     cout  << jump "here";
+     return 0;
+
+}
